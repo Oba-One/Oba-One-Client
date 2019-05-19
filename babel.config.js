437d5d1bfd429830e8a16b2b58af419a6d35e6ab
01 <oba-one@01s-MacBook-Pro.local>
2019-05-18 19:22:24 -0700

    added config files

---
+++ babel.config.js
@@ -0,0 +1,38 @@
+const isTest = String(process.env.NODE_ENV) === 'test';
+
+module.exports = ({ cache, env }) => {
+	const NODE_ENV = env();
+	console.log(NODE_ENV);
+	cache.using(() => process.env.NODE_ENV === 'development');
+	return {
+		presets: [
+			[
+				'@babel/preset-env',
+				{
+					targets: {
+						esmodules: true,
+					},
+					modules: isTest ? 'commonjs' : false,
+				},
+			],
+			'@babel/preset-react',
+		],
+		plugins: [
+			'babel-plugin-styled-components',
+			'@babel/plugin-proposal-class-properties',
+			'@babel/plugin-syntax-dynamic-import',
+			'react-hot-loader/babel',
+			isTest ? 'dynamic-import-node' : null,
+		].filter(Boolean),
+		env: {
+			development: {
+				only: ['src'],
+				plugins: ['babel-plugin-styled-components', 'react-hot-loader/babel'],
+			},
+			production: {
+				only: ['src'],
+				plugins: ['babel-plugin-styled-components'],
+			},
+		},
+	};
+};
