6dc109bd024fc43cacf01287fd7d90bc1e9ee3bf
01 <oba-one@01s-MacBook-Pro.local>
2019-05-27 15:20:31 -0700

    updated profile comps

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,7 +1,12 @@
 // REACT IMPORTS
 import React from 'react';
+import styled from 'styled-components';
 
 import { Container, Grid } from '../../components/global';
+
+import Avatar from '../../components/global/Avatar';
+import Navigation from '../../components/containers/profile/Navigation';
+import Settings from '../../components/containers/profile/Settings';
 
 const ProfileGrid = styled(Grid)``;
 
