db424e9afd38570686643da0ebaa4aee1e24fc67
01 <oba-one@01s-MacBook-Pro.local>
2019-05-27 15:09:17 -0700

    reordered imports & added webpack magic comments. added ui comps as well

--- src/App.js
+++ src/App.js
@@ -1,54 +1,62 @@
 // REACT IMPORTS
+import { hot } from 'react-hot-loader/root';
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { hot } from 'react-hot-loader/root';
 
 // APOLLO IMPORTS
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 // STYLES IMPORTS
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 
+// GLOBAL UI COMPONENTS
+import Header from './components/header';
+import Notifications from './components/notifications';
+import Compass from './components/compass';
+
 // LAZY LOADED ROUTES
-const Landing = lazy(() => import('./containers/Landing'));
-const Home = lazy(() => import('./containers/Home'));
-const Profile = lazy(() => import('./containers/Profile'));
-const Air = lazy(() => import('./containers/Air'));
-const Earth = lazy(() => import('./containers/Earth'));
-const Water = lazy(() => import('./containers/Water'));
+const Landing = lazy(() => import(/* webpackPrefetch: false */ './containers/Landing'));
+const Home = lazy(() => import(/* webpackPrefetch: false */ './containers/Home'));
+const Profile = lazy(() => import(/* webpackPrefetch: false */ './containers/Profile'));
+const Air = lazy(() => import(/* webpackPrefetch: false */ './containers/Air'));
+const Earth = lazy(() => import(/* webpackPrefetch: false */ './containers/Earth'));
+const Water = lazy(() => import( /* webpackPrefetch: false */ './containers/Water'));
 
 const client = new ApolloClient({
-	uri: ''
-})
+	uri: '',
+});
 
 const App = () => {
 	return (
-		<Router
-			getUserConfirmation={null}
-			his
-		>
+		<Router>
 			<ApolloProvider client={client}>
 				<ThemeProvider theme={theme}>
 					<Suspense fallback={<div>Loading</div>}>
-						<nav> Full Width Top Navigation loads if desktop or tablet</nav>
-						<aside>Notifications loads if not landing page</aside>
+						<Header />
+						<Notifications />
 						<Switch>
 							<Route exact path="/" render={props => <Landing {...props} />} />
 							<Route exact path="/home" render={props => <Home {...props} />} />
 							<Route exact path="/air" render={props => <Air {...props} />} />
-							<Route exact path="/earth" render={props => <Earth {...props} />} />
-							<Route exact path="/water" render={props => <Water {...props} />} />
+							<Route
+								exact
+								path="/earth"
+								render={props => <Earth {...props} />}
+							/>
+							<Route
+								exact
+								path="/water"
+								render={props => <Water {...props} />}
+							/>
 							<Route
 								exact
 								path="/profile"
 								render={props => <Profile {...props} />}
 							/>
 						</Switch>
-						<nav>
-							centered Bottom Navigation loads if not landing or settings
-						</nav>
+						<Compass />
 					</Suspense>
 				</ThemeProvider>
 			</ApolloProvider>
