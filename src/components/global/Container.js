bb4670d7b9cdaf937872e54c604419365749c2ed
01 <oba-one@01s-MacBook-Pro.local>
2019-05-27 15:22:28 -0700

    updated container comp

--- src/components/global/Container.js
+++ src/components/global/Container.js
@@ -3,17 +3,41 @@ import styled from 'styled-components';
 
 const Section = styled.section`
     position: relative;
-    display: flex;
-    justify-content: center;
+    margin: 0 auto;
+	min-height: 100vh;
     width: 100%;
-    min-height: 80vh;
-    max-width: 1920px;
-`
+    border: 3px solid red;
+    max-width: ${({ theme }) => theme.breakpoint.desktop};
+    padding-top: ${({ paddingTop }) => paddingTop};
+    padding-bottom: ${({ paddingBottom }) => paddingBottom};
+    @media (max-width: ${({ theme }) => theme.breakpoint.desktop}) {
+        padding-left: ${({ desktopPadding }) => desktopPadding};
+        padding-right: ${({ desktopPadding }) => desktopPadding};
+    }
+    @media (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+        padding-left: ${({ tabletPadding }) => tabletPadding};
+        padding-right: ${({ tabletPadding }) => tabletPadding};
+    }
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+        padding-left: ${({ mobilePadding }) => mobilePadding};
+        padding-right: ${({ mobilePadding }) => mobilePadding};
+    }
+`;
 
-const Container = () => (
-    <Section>
+const Container = ({
+	mobilePadding = '0',
+	tabletPadding = '0',
+	desktopPadding = '32px',
+	paddingTop = '0',
+	paddingBottom = '0',
+}) => (
+	<Section
+		mobilePadding={mobilePadding}
+		tabletPadding={tabletPadding}
+		desktopPadding={desktopPadding}
+		paddingTop={paddingTop}
+		paddingBottom={paddingBottom}
+	/>
+);
 
-    </Section>
-)
-
-export default Container
+export default Container;
