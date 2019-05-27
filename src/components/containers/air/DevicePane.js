2171afadfd520f9d01d89032f9c9cdaeed560046
01 <oba-one@01s-MacBook-Pro.local>
2019-05-27 15:18:01 -0700

    updated air comp

---
+++ src/components/containers/air/DevicePane.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Pane = styled.div`
+
+`
+
+const DevicePane = ({})  => {
+    return (
+        <Pane>
+            Air
+        </Pane>
+    )
+};
+
+export default DevicePane;
