a92bcea4f1cf3cc8251da2d17e7f177282ef7a20
01 <oba-one@01s-MacBook-Pro.local>
2019-06-04 01:24:14 -0700

    updated header comps

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
+import { useSpring, animated } from 'react-spring';
 import { Avatar } from '../global';
 
-const StyledHeader = styled.header`
+import useResizeObserver from '../../helpers/useResizeObserver';
+
+const AnimatedHeader = styled(animated.header)`
 	position: fixed;
 	display: flex;
 	z-index: 1007;
@@ -10,28 +14,35 @@ const StyledHeader = styled.header`
 	justify-content: space-between;
 	padding: 0 27px;
 	width: 100%;
-	height: 80px;
-	background: ${({theme}) => theme.app.color.primary};
-	box-shadow: 0px 2px 6px 0px rgba(0,0,0,.12);
+	height: 60px;
+	background: ${({ theme }) => theme.app.color.primary};
+	box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.12);
 	border-bottom: solid 1px #dadce0;
 	will-change: opacity transform;
 `;
 
-const Logo = styled.div`
+const Logo = styled.div``;
 
-`;
+const Profile = styled.div``;
 
-const Profile = styled.div`
+const Header = ({ location }) => {
+	const isLanding = location.pathname === '/landing';
+	const isTablet = useResizeObserver() <= 920;
 
-`;
+	const animationStyle = useSpring({
+		
+	});
 
-const Header = ({}) => {
+	if (isLanding || isTablet) {
+		return null;
+	}
+
 	return (
-		<StyledHeader>
+		<AnimatedHeader style={animationStyle}>
 			<Logo>Logo</Logo>
 			<Profile>Profile</Profile>
-		</StyledHeader>
+		</AnimatedHeader>
 	);
 };
 
-export default Header;
+export default withRouter(Header);
