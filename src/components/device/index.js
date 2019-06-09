f9c6d936e5ff29721cdbc32f461c4ebb31c6d875
01 <oba-one@01s-MacBook-Pro.local>
2019-06-09 13:22:53 -0700

    created device comp

---
+++ src/components/device/index.js
@@ -0,0 +1,20 @@
+import React, { Suspense, lazy } from 'react';
+
+import Pane from './Pane';
+import Init from './Init';
+
+const Dashboard = lazy(() => import('./Dashboard'));
+const Remote = lazy(() => import('./Remote'));
+const Stream = lazy(() => import('./Stream'));
+
+const DevicePane = ({}) => {
+	return (
+		<Pane>
+			<Suspense fallback={<div>Loading Device Page</div>}>
+				<Init/>
+			</Suspense>
+		</Pane>
+	);
+};
+
+export default DevicePane;
