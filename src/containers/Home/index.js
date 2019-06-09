53fd7999659722676aef686a88f1586c53819c4f
01 <oba-one@01s-MacBook-Pro.local>
2019-06-09 13:19:28 -0700

    updated home container

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,26 +1,49 @@
 // REACT IMPORTS
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import styled from 'styled-components';
 
 import { Container, Grid } from '../../components/global';
 
-import Activity from '../../components/home/Activity';
-import ImageGallery from '../../components/home/ImageGallery';
-import YoutubeFeed from '../../components/home/YoutubeFeed';
+// LAZY LOADED COMPONENTS
+const Youtube = lazy(() => import('../../components/home/Youtube'));
+const Spotify = lazy(() => import('../../components/home/Spotify'));
+const Activity = lazy(() => import('../../components/home/Activity'));
 
-
-const HomeGrid = styled(Grid)``;
+const HomeGrid = styled(Grid)`
+	padding-top: 60px;
+	padding-bottom: 120px;
+	grid-template-areas:
+		'activity activity activity activity activity activity'
+		'utube utube utube utube . .'
+		'. . spotify spotify spotify spotify';
+	@media (max-width: 920px) {
+		padding-top: 0;
+		grid-template-areas:
+			'feed feed feed feed'
+			'activity activity activity activity'
+			'utube utube spotify spotify';
+	}
+	@media (max-width: 480px) {
+		grid-template-areas:
+			'feed feed '
+			'activity activity'
+			'utube utube '
+			'spotify spotify'
+			'profile profile';
+	}
+`;
 
 const Home = () => {
 	return (
-		<Container>
-			<HomeGrid>
-				<div>Last Session</div>
-				<div>Activity List</div>
-				<div>Image Gallery</div>
-				<div>Youtube Feed</div>
-			</HomeGrid>
-		</Container>
+		<Suspense fallback={<div>loading section...</div>}>
+			<Container>
+				<HomeGrid>
+					<Activity />
+					<Youtube />
+					<Spotify />
+				</HomeGrid>
+			</Container>
+		</Suspense>
 	);
 };
 
