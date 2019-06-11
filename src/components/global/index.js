1da3a1ea951e6ede94f4732b67f7f8f2631ba943
01 <oba-one@01s-MacBook-Pro.local>
2019-06-11 14:48:56 -0700

    updated global comps

--- src/components/global/index.js
+++ src/components/global/index.js
@@ -1,9 +1,7 @@
 import Avatar from './Avatar';
 import Container from './Container';
 import Grid from './Grid';
-import Loader from './Loader';
-
 import Wrapper from './Wrapper';
-import DevicePane from './DevicePane';
 
-export { Avatar, Container, Grid, Loader, Wrapper, DevicePane };
+
+export { Avatar, Container, Grid, Wrapper};
