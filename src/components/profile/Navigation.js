5696e108dd02e15aa3ff5af36815e078c1437bca
01 <oba-one@01s-MacBook-Pro.local>
2019-06-11 23:17:21 -0700

    updated profile comps

--- src/components/profile/Navigation.js
+++ src/components/profile/Navigation.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
+import { useSpring, animated } from 'react-spring';
 
-const Nav = styled.nav`
+const Nav = styled(animated.nav)`
 	position: sticky;
 	display: flex;
+	z-index: 1007;
 	align-items: center;
+	justify-content: space-between;
+	padding: 0 27px;
 	width: 100%;
 	height: 60px;
+	background: ${({ theme }) => theme.app.color.primary};
+	box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.12);
+	border-bottom: solid 1px #dadce0;
+	will-change: opacity transform;
 `;
 
-const Navigation = ({}) => {
+const Navigation = ({ location }) => {
 	return <Nav> ProfileNavigation</Nav>;
 };
 
-export default Navigation;
+export default withRouter(Navigation);
---
+++ src/components/profile/Settings.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Settings = ({}) => {
+	return <div>Icon</div>;
+};
+
+export default Settings;
