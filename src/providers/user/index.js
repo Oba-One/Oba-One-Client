f96dec3a13d6e91770bd648a0a33ca253646e4f9
01 <oba-one@01s-MacBook-Pro.local>
2019-07-17 21:02:50 -0700

    updated user provider

--- src/providers/user/index.js
+++ src/providers/user/index.js
@@ -1,8 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
+import {useQuery, useMutation} from '@apollo/react-hooks';
 import { UserProvider as Provider } from './context';
+import {
+	userReducer,
+	GET_USER,
+	LOG_IN,
+	LOG_OUT,
+	UPDATE_USER,
+	DELETE_USER,
+} from './reducers';
+import provider from '../mockData';
 
 const UserProvider = ({ children }) => {
-	return <Provider value={{}}>{children}</Provider>;
+	const [user, dispatch] = useReducer(userReducer, provider.user);
+
+	const getUser = id => {
+		dispatch({
+			type: GET_USER,
+			id: id,
+		});
+	};
+
+	const logIn = provider => {
+		dispatch({
+			type: LOG_IN,
+			provider: provider,
+		});
+	};
+
+	const logOut = provider => {
+		dispatch({
+			type: LOG_OUT,
+			provider: provider,
+		});
+	};
+
+	const updateUser = user => {
+		dispatch({
+			type: UPDATE_USER,
+			user: user,
+		});
+	};
+
+	const deleteUser = id => {
+		dispatch({
+			type: DELETE_USER,
+			id: id,
+		});
+	};
+
+	return (
+		<Provider
+			value={{
+				profile: user,
+				getUser: getUser,
+				logIn: logIn,
+				logOut: logOut,
+				updateUser: updateUser,
+				deleteUser: deleteUser,
+			}}
+		>
+			{children}
+		</Provider>
+	);
 };
 
 export default UserProvider;
--- src/providers/user/reducers.js
+++ src/providers/user/reducers.js
@@ -1,46 +1,64 @@
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import {
+	GET_USER_QUERY,
+	LOG_IN_MUTATION,
+	LOG_OUT_MUTATION,
+	UPDATE_USER_MUTATION,
+	DELETE_USER_MUTATION,
+} from './queries';
+
 const GET_USER = 'GET_USER';
 const LOG_IN = 'LOG_IN';
 const LOG_OUT = 'LOG_OUT';
 const UPDATE_USER = 'UPDATE_USER';
 const DELETE_USER = 'DELETE_USER';
 
-const getUser = () => {};
+const getUser = (id = ``, state = {}) => {
+	console.log(state);
+	console.log(id);
 
-const logIn = provider => {
-	// Firebase Auth for Google and Github
-	if (provider === 'google') {
-		// Google Auth Flow
-	} else if (provider === 'github') {
-	} else {
-		return;
-	}
+	return state;
 };
 
-const logOut = token => {
-	console.log(token);
-	// Firebase Log Out Callback here
+const logIn = (provider, state) => {
+	console.log(state);
+	console.log(provider);
+
+	return state;
 };
 
-const updateUser = () => {
-    // Send Mutation & Update User Context
+const logOut = (user, state) => {
+	console.log(user);
+	console.log(provider);
+
+	return state;
 };
 
-const deleteUser = () => {
-    // Send Mutation & Update User Context
+const updateUser = (user, state) => {
+	console.log(user);
+	console.log(provider);
+
+	return state;
 };
 
+const deleteUser = (id = ``, state = {}) => {
+	console.log(state);
+	console.log(id);
+
+	return state;
+};
 const userReducer = (state, action) => {
 	switch (action.type) {
 		case GET_USER:
-			break;
+			return getUser(action.id, state);
 		case LOG_IN:
-			break;
+			return logIn(action.provider, state);
 		case UPDATE_USER:
-			break;
+			return updateUser(action.user, state);
 		case LOG_OUT:
-			break;
+			return logOut(action.user, state);
 		case DELETE_USER:
-			break;
+			return deleteUser(id, state);
 		default:
 			return state;
 	}
